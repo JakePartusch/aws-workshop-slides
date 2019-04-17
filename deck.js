b11d5a218a51f0578bd80f7a23d3f309df561993
Jake Partusch <jakepartusch@gmail.com>
2019-04-16 20:43:00 -0500

    feat: adding more slide content

--- deck.js
+++ deck.js
@@ -3,6 +3,16 @@ import { slides as awsIntro } from "./topics/aws-intro.mdx";
 import { slides as lex } from "./topics/lex.mdx";
 import { slides as dynamo } from "./topics/dynamo.mdx";
 import { slides as lambda } from "./topics/lambda.mdx";
+import { slides as awsConclusion } from "./topics/aws-conclusion.mdx";
+import { slides as demo } from "./topics/demo.mdx";
 
 export { default as theme } from "@jakepartusch/opi-mdx-theme";
-export const slides = [...intro, ...awsIntro, ...lex, ...dynamo, ...lambda];
+export const slides = [
+  ...intro,
+  ...awsIntro,
+  ...lex,
+  ...dynamo,
+  ...lambda,
+  ...awsConclusion,
+  ...demo
+];
