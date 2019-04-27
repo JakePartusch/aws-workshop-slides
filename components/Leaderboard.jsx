33a1b28e77ef0deb69f1f44929313330e34467d7
Jake Partusch <jakepartusch@gmail.com>
2019-04-26 22:48:12 -0500

    feat: create leaderboard slide

---
+++ components/Leaderboard.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import useInterval from "@use-it/interval";
+import axios from "axios";
+
+const LeaderboardItem = styled.div({
+  display: "flex"
+});
+
+const Username = styled.div({
+  marginLeft: "1.5rem"
+});
+
+const TotalUsers = styled.div({
+  position: "absolute",
+  top: 0,
+  right: 0,
+  margin: "0 4rem"
+});
+
+const Leaderboard = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      "https://sg1xdt2fv7.execute-api.us-east-1.amazonaws.com/dev/quizzes"
+    );
+    setUsers(data);
+  };
+
+  useInterval(() => {
+    fetchData();
+  }, 3000);
+
+  const correctAnswers = ["A", "C", "B", "B", "D"];
+
+  const usersWithScores = users.map(user => {
+    const score = user.answers.reduce((total, answer, i) => {
+      const answerScore = answer === correctAnswers[i] ? 100 : 0;
+      total += answerScore;
+      return total;
+    }, 0);
+    const timeScore = Math.max(
+      100 -
+        Math.round(
+          (new Date(user.completionTime) - new Date(user.startTime)) / 1000
+        ),
+      0
+    );
+    return {
+      username: user.username,
+      score: score + timeScore
+    };
+  });
+
+  const sortedUsers = usersWithScores.sort((a, b) =>
+    a.score < b.score ? 1 : -1
+  );
+
+  const emojiMedals = ["🎉", "🥈", "🥉"];
+
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      {sortedUsers.map((user, index) => (
+        <>
+          {index < 3 && (
+            <LeaderboardItem>
+              <div>{emojiMedals[index]}</div>
+              <Username>{`${user.username} - ${user.score}`}</Username>
+            </LeaderboardItem>
+          )}
+        </>
+      ))}
+      <TotalUsers>
+        <h2>{sortedUsers.length}</h2>
+      </TotalUsers>
+    </div>
+  );
+};
+
+export default Leaderboard;
